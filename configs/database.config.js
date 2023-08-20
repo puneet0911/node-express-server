a8f3e0a5aac050923cf372e0ce8e224cab19e2c2
Puneet Sharma <puneetpremsharma@hotmail.com>
2023-08-20 14:44:43 +0530

    added the mongoDB connection file with mongoose

---
+++ configs/database.config.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+let mongoose = require('mongoose');
+
+// These details will be required to connect mongoDB which is hosted in sever
+const username = process.env.MONGODB_USERNAME; // REPLACE WITH YOUR Cluster Username
+const password = process.env.MONGODB_PASSWORD; // REPLACE WITH YOUR Cluster Password
+const cluster = process.env.MONGODB_CLUSTER; // REPLACE WITH YOUR Cluster NAme
+const dbname = process.env.MONGODB_DB; // REPLACE WITH YOUR DB NAME
+
+class Database {
+  constructor() {
+    this._connect();
+  }
+
+  _connect() {
+    mongoose
+      .connect(`mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}`)
+      .then(() => {
+        console.log('Database connection successful');
+      })
+      .catch((err) => {
+        console.log(" err ",err)
+        console.error('Database connection error');
+      });
+  }
+}
+
+module.exports = new Database();
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express ();
 const indexRouter = require('./Routes/index')
+const database = require('./configs/database.config')
 app.use(express.json());
-
 app.use("/", indexRouter);
 module.exports = app;
