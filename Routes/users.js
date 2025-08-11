7a4ebb1ec8880ec06a27bf717c9d3ab68a9f0b10
Puneet Sharma <puneetpremsharma@hotmail.com>
2025-08-11 23:35:48 +0530

    Update user controller function with routes

--- Routes/users.js
+++ Routes/users.js
@@ -6,38 +6,16 @@ const userModel = require("../Models/user.model");
 const userController = require('../controllers/user.controller');
 
 /* GET all Users. */
-router.get('/all', async function(req, res, next) {
-    try{
-        const allUsers = await userModel.find()
-        res.status(200).send({
-            status: 'Success',
-            data: allUsers,
-        })
-    }catch (error){
-        console.log(" error ", error)
-    }
-});
+router.get('/all', userController.getAllUsers);
 
 /* Get specific user. */
-router.get('/:email', async function(req, res, next) {
-    try{
-        const userRep = await userModel.find({email:req.params.email })
-        res.status(200).send({
-            status: 'Success',
-            data: userRep,
-        })
-    }catch (error){
-        console.log(" error ", error)
-    }
-});
+router.get('/:email', userController.getUserDetails);
 
 /* Get specific user. */
 router.post('/signup', userController.signup);
 
 // Login route
-router.post('/login',(req, res, next) => {
-    
-});
+router.post('/login',userController.login);
 
   
 module.exports = router;
