8cf62cc91819eaa4eb80e6740ea6aede8aed8215
Puneet Sharma <puneetpremsharma@hotmail.com>
2025-08-14 00:24:59 +0530

    Added the db connection according to the environment

--- configs/database.config.js
+++ configs/database.config.js
@@ -6,13 +6,19 @@ const username = process.env.MONGODB_USERNAME; // REPLACE WITH YOUR Cluster User
 const password = process.env.MONGODB_PASSWORD; // REPLACE WITH YOUR Cluster Password
 const cluster = process.env.MONGODB_CLUSTER; // REPLACE WITH YOUR Cluster NAme
 const dbname = process.env.MONGODB_DB; // REPLACE WITH YOUR DB NAME
-
+const url = process.env.MONGODB_URL; // REPLACE WITH YOUR DB NAME
+ 
   const connectDB = async () => {
     try {
+      let connectionString = `mongodb+srv://${username}:${password}@${cluster}/${dbname}?retryWrites=true&w=majority`;
+      if(process.env.__DEV__ === 'true') { 
+        connectionString = `${url}${dbname}`;
+        console.log(" connectionString ", connectionString)  
+      }
       await mongoose
-        .connect(`mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}`,{
+        .connect(connectionString,{
           useNewUrlParser: true,
-          useUnifiedTopology: true,
+          useUnifiedTopology: true,  
         })
         .then(() => {
           console.log('Database connection successful');
