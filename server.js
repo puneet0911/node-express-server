0f023a829f8f5dbbe7b07d2238edd4bc15d019a1
Puneet Sharma <puneetpremsharma@hotmail.com>
2025-08-18 23:54:22 +0530

    Correction in global error handler

--- server.js
+++ server.js
@@ -116,7 +116,10 @@ function requireRole(role){
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ error: 'Something went wrong!' });
+  res.status(500).json({
+    error: 'Something went wrong!',
+    message: process.env.NODE_ENV === 'development' ? err.message : undefined
+  });
 });
 
 module.exports = app;
